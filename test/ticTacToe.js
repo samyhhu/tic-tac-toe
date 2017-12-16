5e3054dfbedf32ff10fb6570de2862cd70356059
Jared Scheib <jared.scheib@gmail.com>
2017-12-16 15:07:41 -0800

    Create skeleton for game flow tests

--- test/ticTacToe.js
+++ test/ticTacToe.js
@@ -28,100 +28,114 @@ contract('TicTacToe', async function(accounts) {
     await reverter.snapshot() // Create first `snapshot` before all tests
   })
 
-  beforeEach('reset ownerState, otherState, and gameState to 0', async () => {
-    await ticTacToe.setGameState(0)
-    await ticTacToe.setOwnerState(0)
-    await ticTacToe.setOtherState(0)
+  describe('Basic Moves', () => {
+    beforeEach('reset ownerState, otherState, and gameState to 0', async () => {
+      await ticTacToe.setGameState(0)
+      await ticTacToe.setOwnerState(0)
+      await ticTacToe.setOtherState(0)
 
-    const gameState = await ticTacToe.gameState()
-    const ownerState = await ticTacToe.ownerState()
-    const otherState = await ticTacToe.otherState()
-    assert.equal(gameState, 0)
-    assert.equal(ownerState, 0)
-    assert.equal(otherState, 0)
-  })
-
-  describe('Negative', () => {
-    it('should NOT allow to call main function by non-owner', async () => {
-      const result = await ticTacToe.main.call(1, {from: NON_OWNER})
-      assert.isFalse(result)
-    })
-
-    it('should NOT change state when calling main function by non-owner', async () => {
-      const initialState = await ticTacToe.ownerState()
-
-      asserts.equal(initialState, 0)
-
-      await ticTacToe.main(2, {from: NON_OWNER})
-      const currentState = await ticTacToe.ownerState()
-      asserts.equal(currentState, 0)
-    })
-
-    it('should NOT emit event about state changes when calling main function by non-owner, should emit error event instead', async () => {
-      const tx = await ticTacToe.main(3, {from: NON_OWNER})
-      asserts.equal(tx.logs.length, 1)
-      asserts.equal(tx.logs[0].address, ticTacToe.address)
-      asserts.equal(tx.logs[0].event, 'Error')
-      asserts.equal(tx.logs[0].args.msg, 'You are not the owner')
-    })
-
-    it('should revert transaction when calling other function by contract owner', async () => {
-      const initialState = await ticTacToe.ownerState()
-      asserts.equal(initialState, 0)
-
-      asserts.throws(ticTacToe.other(5, {from: OWNER}))
-      const currentState = await ticTacToe.ownerState()
-      asserts.equal(currentState, 0)
-    })
-  })
-
-  describe('Positive', () => {
-    it('should initialize with gameState 0 (playable)', async () => {
       const gameState = await ticTacToe.gameState()
+      const ownerState = await ticTacToe.ownerState()
+      const otherState = await ticTacToe.otherState()
       assert.equal(gameState, 0)
+      assert.equal(ownerState, 0)
+      assert.equal(otherState, 0)
     })
 
-    it('should allow contract owner to call main function', async () => {
-      const result = await ticTacToe.main.call(1, {from: OWNER})
-      assert.isTrue(result)
+    describe('Negative', () => {
+      it('should NOT allow to call main function by non-owner', async () => {
+        const result = await ticTacToe.main.call(1, {from: NON_OWNER})
+        assert.isFalse(result)
+      })
+
+      it('should NOT change state when calling main function by non-owner', async () => {
+        const initialState = await ticTacToe.ownerState()
+
+        asserts.equal(initialState, 0)
+
+        await ticTacToe.main(2, {from: NON_OWNER})
+        const currentState = await ticTacToe.ownerState()
+        asserts.equal(currentState, 0)
+      })
+
+      it('should NOT emit event about state changes when calling main function by non-owner, should emit error event instead', async () => {
+        const tx = await ticTacToe.main(3, {from: NON_OWNER})
+        asserts.equal(tx.logs.length, 1)
+        asserts.equal(tx.logs[0].address, ticTacToe.address)
+        asserts.equal(tx.logs[0].event, 'Error')
+        asserts.equal(tx.logs[0].args.msg, 'You are not the owner')
+      })
+
+      it('should revert transaction when calling other function by contract owner', async () => {
+        const initialState = await ticTacToe.ownerState()
+        asserts.equal(initialState, 0)
+
+        asserts.throws(ticTacToe.other(5, {from: OWNER}))
+        const currentState = await ticTacToe.ownerState()
+        asserts.equal(currentState, 0)
+      })
     })
 
-    it('should change state when calling main function by contract owner', async () => {
-      await ticTacToe.main(2, {from: OWNER})
+    describe('Positive', () => {
+      it('should initialize with gameState 0 (playable)', async () => {
+        const gameState = await ticTacToe.gameState()
+        assert.equal(gameState, 0)
+      })
 
-      const currentOwnerState = await ticTacToe.ownerState()
-      const currentOtherState = await ticTacToe.otherState()
+      it('should allow contract owner to call main function', async () => {
+        const result = await ticTacToe.main.call(1, {from: OWNER})
+        assert.isTrue(result)
+      })
 
-      asserts.equal(currentOwnerState, 2)
-      asserts.equal(currentOtherState, 0)
+      it('should change state when calling main function by contract owner', async () => {
+        await ticTacToe.main(2, {from: OWNER})
+
+        const currentOwnerState = await ticTacToe.ownerState()
+        const currentOtherState = await ticTacToe.otherState()
+
+        asserts.equal(currentOwnerState, 2)
+        asserts.equal(currentOtherState, 0)
+      })
+
+      it('should emit event about state changes when calling main function by contract owner', async () => {
+        const tx = await ticTacToe.main(1, {from: OWNER})
+        asserts.equal(tx.logs.length, 1)
+        asserts.equal(tx.logs[0].address, ticTacToe.address)
+        asserts.equal(tx.logs[0].event, 'StateChanged')
+        asserts.equal(tx.logs[0].args.ownerState, 1)
+        asserts.equal(tx.logs[0].args.otherState, 0)
+      })
+
+      it('should change state when calling other function by non-owner', async () => {
+        await ticTacToe.other(2, {from: NON_OWNER})
+
+        const currentOwnerState = await ticTacToe.ownerState()
+        const currentOtherState = await ticTacToe.otherState()
+
+        asserts.equal(currentOwnerState, 0)
+        asserts.equal(currentOtherState, 2)
+      })
+
+      it('should emit event about state changes when calling other function by non-owner', async () => {
+        const tx = await ticTacToe.other(2, {from: NON_OWNER})
+        asserts.equal(tx.logs.length, 1)
+        asserts.equal(tx.logs[0].address, ticTacToe.address)
+        asserts.equal(tx.logs[0].event, 'StateChanged')
+        asserts.equal(tx.logs[0].args.ownerState, 0)
+        asserts.equal(tx.logs[0].args.otherState, 2)
+      })
     })
+  })
 
-    it('should emit event about state changes when calling main function by contract owner', async () => {
-      const tx = await ticTacToe.main(1, {from: OWNER})
-      asserts.equal(tx.logs.length, 1)
-      asserts.equal(tx.logs[0].address, ticTacToe.address)
-      asserts.equal(tx.logs[0].event, 'StateChanged')
-      asserts.equal(tx.logs[0].args.ownerState, 1)
-      asserts.equal(tx.logs[0].args.otherState, 0)
-    })
-
-    it('should change state when calling other function by non-owner', async () => {
-      await ticTacToe.other(2, {from: NON_OWNER})
-
-      const currentOwnerState = await ticTacToe.ownerState()
-      const currentOtherState = await ticTacToe.otherState()
-
-      asserts.equal(currentOwnerState, 0)
-      asserts.equal(currentOtherState, 2)
-    })
-
-    it('should emit event about state changes when calling other function by non-owner', async () => {
-      const tx = await ticTacToe.other(2, {from: NON_OWNER})
-      asserts.equal(tx.logs.length, 1)
-      asserts.equal(tx.logs[0].address, ticTacToe.address)
-      asserts.equal(tx.logs[0].event, 'StateChanged')
-      asserts.equal(tx.logs[0].args.ownerState, 0)
-      asserts.equal(tx.logs[0].args.otherState, 2)
-    })
+  describe('Game Flows', () => {
+    it('should reject invalid uint16 representations of board positions by owner', async () => {})
+    it('should reject invalid uint16 representations of board positions by other', async () => {})
+    it('should reject two sequential moves by owner', async () => {})
+    it('should reject two sequential moves by other', async () => {})
+    it('should reject multiple simultaneous moves by owner', async () => {})
+    it('should reject multiple simultaneous moves by other', async () => {})
+    it('should allow & log other victory', async () => {})
+    it('should allow & log other victory', async () => {})
+    it('should allow & log draw', async () => {})
   })
 })
