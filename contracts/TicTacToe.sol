cfa3f8b20fa0be004715ae29578f3d91147223da
Ben Woosley <ben.woosley@gmail.com>
2017-12-15 21:49:18 -0800

    Stub out valid and victory methods, split state to owner and other state

--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -5,33 +5,56 @@ contract TicTacToe {
     address owner;
 
     // Storage variable must be public if we want automatically have a getter for it
-    uint256 public state;
+    uint16 public ownerState;
+    uint16 public otherState;
 
-    event StateChanged(uint256 changedTo);
+    event StateChanged(uint16 ownerState, uint16 otherState);
     event Error(string msg);
 
     function TicTacToe() public {
         owner = msg.sender;
     }
 
+    function valid(uint16 state, uint16 _newState) private returns(bool) {
+      return true;
+    }
+
+    function victory(uint16 state) private returns(bool) {
+      return false;
+    }
+
     // Function to test return value, state changes and event emission
-    function main(uint256 _newState) public returns(bool) {
+    function main(uint16 _newState) public returns(bool) {
         if (msg.sender != owner) {
-            Error("You are not the owner");
-            return false;
+          Error("You are not the owner");
+          return false;
         }
-        state = _newState;
-        StateChanged(_newState);
+        if (!valid(ownerState, _newState)) {
+          Error("Invalid move");
+          return false;
+        }
+        ownerState = _newState;
+        if (victory(ownerState)) {
+          // Victory
+        }
+        StateChanged(ownerState, otherState);
         return true;
     }
 
     // Function to test VM exception
-    function other(uint256 _newState) public returns(bool) {
-        state = _newState;
-        StateChanged(_newState);
-        // Require goes after state changes in order to demonstrate changes reversion
-        require(msg.sender != owner);
-        return true;
+    function other(uint16 _newState) public returns(bool) {
+      if (!valid(otherState, _newState)) {
+        Error("Invalid move");
+        return false;
+      }
+      otherState = _newState;
+      if (victory(ownerState)) {
+        // Victory
+      }
+      StateChanged(ownerState, otherState);
+      // Require goes after state changes in order to demonstrate changes reversion
+      require(msg.sender != owner);
+      return true;
     }
 
 }
